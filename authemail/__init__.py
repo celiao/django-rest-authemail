d71ccb3b2406a0b3e8d1ae137d261bdeb08be69e
Celia Oakley <celia.oakley@alumni.stanford.edu>
2020-08-16 14:08:52 -0700

    Update to version 1.5.1

--- authemail/__init__.py
+++ authemail/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django-rest-authemail'
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 __author__ = 'Celia Oakley'
 __copyright__ = 'Copyright (c) 2014-2020 Celia Oakley'
 __license__ = 'GPLv3'
