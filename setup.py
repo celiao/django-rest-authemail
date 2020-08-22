1e04597582dd23a1635941df934ba56bf949d849
Celia Oakley <celia.oakley@alumni.stanford.edu>
2020-08-22 11:06:50 -0700

    Make compatible with Python 3.7

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        'Django==1.11',
+        'Django==1.11.17',
         'djangorestframework==3.7.0',
         'requests>=2.3.0',
     ],
