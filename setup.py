354a599b6caf99e2d7b3f7b29f7951a02925111d
Celia Oakley <celia.oakley@alumni.stanford.edu>
2020-12-30 22:41:47 +1100

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Operating System :: OS Independent',
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
