eaf7d90564c16d7ba4b3b8d383d136f1bcdaa7cb
Johan Backman <johan@cultivate.com>
2021-05-14 08:20:08 -0700

    Fix has_add_permission calls for django 3.x

--- authemail/admin.py
+++ authemail/admin.py
@@ -3,97 +3,97 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from authemail.forms import EmailUserCreationForm, EmailUserChangeForm
-from authemail.models import SignupCode, PasswordResetCode, EmailChangeCode
+from authemail.forms import EmailUserChangeForm, EmailUserCreationForm
+from authemail.models import EmailChangeCode, PasswordResetCode, SignupCode
 
 
 class SignupCodeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'user', 'ipaddr', 'created_at')
-    ordering = ('-created_at',)
-    readonly_fields = ('user', 'code', 'ipaddr')
+    list_display = ("code", "user", "ipaddr", "created_at")
+    ordering = ("-created_at",)
+    readonly_fields = ("user", "code", "ipaddr")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class SignupCodeInline(admin.TabularInline):
     model = SignupCode
-    fieldsets = (
-        (None, {
-            'fields': ('code', 'ipaddr', 'created_at')
-        }),
-    )
-    readonly_fields = ('code', 'ipaddr', 'created_at')
+    fieldsets = ((None, {"fields": ("code", "ipaddr", "created_at")}),)
+    readonly_fields = ("code", "ipaddr", "created_at")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class PasswordResetCodeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'user', 'created_at')
-    ordering = ('-created_at',)
-    readonly_fields = ('user', 'code')
+    list_display = ("code", "user", "created_at")
+    ordering = ("-created_at",)
+    readonly_fields = ("user", "code")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class PasswordResetCodeInline(admin.TabularInline):
     model = PasswordResetCode
-    fieldsets = (
-        (None, {
-            'fields': ('code', 'created_at')
-        }),
-    )
-    readonly_fields = ('code', 'created_at')
+    fieldsets = ((None, {"fields": ("code", "created_at")}),)
+    readonly_fields = ("code", "created_at")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class EmailChangeCodeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'user', 'email', 'created_at')
-    ordering = ('-created_at',)
-    readonly_fields = ('user', 'code', 'email')
+    list_display = ("code", "user", "email", "created_at")
+    ordering = ("-created_at",)
+    readonly_fields = ("user", "code", "email")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class EmailChangeCodeInline(admin.TabularInline):
     model = EmailChangeCode
-    fieldsets = (
-        (None, {
-            'fields': ('code', 'email', 'created_at')
-        }),
-    )
-    readonly_fields = ('code', 'email', 'created_at')
+    fieldsets = ((None, {"fields": ("code", "email", "created_at")}),)
+    readonly_fields = ("code", "email", "created_at")
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
 class EmailUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('first_name', 'last_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
     )
     form = EmailUserChangeForm
     add_form = EmailUserCreationForm
     inlines = [SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline]
-    list_display = ('email', 'is_verified', 'first_name', 'last_name',
-                    'is_staff')
-    search_fields = ('first_name', 'last_name', 'email')
-    ordering = ('email',)
+    list_display = ("email", "is_verified", "first_name", "last_name", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("email",)
 
 
 admin.site.register(get_user_model(), EmailUserAdmin)
--- example_project/accounts/admin.py
+++ example_project/accounts/admin.py
@@ -1,26 +1,35 @@
+from authemail.admin import EmailUserAdmin
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
-
-from authemail.admin import EmailUserAdmin
 
 from accounts.models import VerifiedUser
 
 
 class MyUserAdmin(EmailUserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('first_name', 'last_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'is_verified', 'groups',
-                                       'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        (_('Custom info'), {'fields': ('date_of_birth',)}),
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Custom info"), {"fields": ("date_of_birth",)}),
     )
 
 
 class VerifiedUserAdmin(MyUserAdmin):
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
