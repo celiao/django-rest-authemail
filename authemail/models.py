d0c6e3149c2a4290c418ccf842d3a84aa96946ff
Seraphin <svandegar@hotmail.com>
2022-03-17 16:48:09 -0300

    Replace by ugettext_lazy by gettext_lazy to work with Django 4

--- authemail/models.py
+++ authemail/models.py
@@ -7,7 +7,7 @@ from django.core.mail.message import EmailMultiAlternatives
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.mail import send_mail
 
 # Make part of the model eventually, so it can be edited
