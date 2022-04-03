6b79d902059cf2a72948c388f0501e6365284cfc
Celia Oakley <celia.oakley@alumni.stanford.edu>
2022-04-02 23:02:09 -0700

    Change remaining occurrences of ugettext_lazy to gettext_lazy

--- authemail/forms.py
+++ authemail/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class EmailUserCreationForm(forms.ModelForm):
--- example_project/example_project/forms.py
+++ example_project/example_project/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.forms import NON_FIELD_ERRORS
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class AddErrorMixin(object):
